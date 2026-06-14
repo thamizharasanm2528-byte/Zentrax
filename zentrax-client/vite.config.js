cd626cd06019ab40987058a65751e32ad3a747cf
thamizharasanm2528-byte <thamizharasanm2528@gmail.com>
2026-06-14 20:16:38 +0530

    Auto-sync: modified vite.config.js

--- zentrax-client/vite.config.js
+++ zentrax-client/vite.config.js
@@ -12,5 +12,25 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     allowedHosts: ['.ngrok-free.dev']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('three') || id.includes('@react-three')) {
+              return 'threejs-vendor';
+            }
+            if (id.includes('firebase')) {
+              return 'firebase-vendor';
+            }
+            if (id.includes('react-syntax-highlighter')) {
+              return 'syntax-highlighter-vendor';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
